607e4a0ae9bbec9da01160ab61a05a8462c72520
lynnadelesadler <lynnadelesadler@yahoo.com>
2024-02-14 15:40:34 -0500

    first commit

---
+++ catch-of-the-day-api/src/test/java/com/catchoftheday/CatchOfTheDayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catchoftheday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchOfTheDayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
